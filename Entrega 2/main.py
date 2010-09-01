d80452265313460024743250bcd12c5128830138
Diego <diegomarcov@gmail.com>
2010-09-01 09:55:49 -0300

    Petit mejora en la salida por pantalla.

--- Entrega 2/main.py
+++ Entrega 2/main.py
@@ -1,3 +1,4 @@
+import sys
 import argparse, io
 from lexan import LexAn,LexError
 
@@ -21,11 +22,16 @@ if __name__ == '__main__':
 	'''
 
 	lexicalAnalyzer = LexAn(inputFile)
-	print "\nStarting file lexical analysis...\n\n%s %s %s\n" % ("LEXEME".center(35), "TOKEN".center(35), "LINE NUMBER".center(20))
+	print "\nStarting file lexical analysis...\n\n"
+	print "----------------------------------------------------------------------------------------------"
+	print "|%s|%s|%s|" % ("LEXEME".center(35), "TOKEN".center(35), "LINE NUMBER".center(20))
+	print "----------------------------------------------------------------------------------------------"
 	token = ""
 	try:
 		while(token != "<EOF>"):
 			token = lexicalAnalyzer.getNextToken()
-			print "%s %s %s" % (str(lexicalAnalyzer.getCurrentLexeme()).center(35), str(token).center(35), str(lexicalAnalyzer.getCurrentLine()).center(20))
+			print "|%s|%s|%s|" % (str(lexicalAnalyzer.getCurrentLexeme()).center(35), str(token).center(35), str(lexicalAnalyzer.getCurrentLine()).center(20))
 	except LexError as e:
 		print e
+	print "----------------------------------------------------------------------------------------------"	
+	
