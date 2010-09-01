e88f585cfda524f8444c6d5dc288abfa9a7d6684
Diego <diegomarcov@gmail.com>
2010-09-01 14:13:35 -0300

    Subido programa.pas

---
+++ Entrega 2/programa.pas
@@ -0,0 +1,40 @@
+{El objetivo del test es ejecutar un programa con todos 
+los simbolos especiales que forman parte del lenguaje.
+Resultado esperado: Test Valido
+Tokens: todos los tokens (ver informe)
+}
+program prog;
+
+	const A = 34;
+	  C = 'c';
+	  B = 55;
+	function p:bloo;
+	begin   end;	
+	var
+		x,y,z:+boolean..16; 
+		y,l:array[+6..-45] of true; 
+		x:booleann;
+	function f1(var z,d,e:integer; m,n : boolean):boolean;
+	begin 
+		if (4+5) div 3 < 5 then	
+			f1:=7;
+		end
+		
+	end;	
+	procedure p2(var z:integer);
+	begin 
+		
+	end;
+	
+begin
+  if (x<y) or (x<=y) and (x>=y) or (x>y) and (not (x=y)) then
+     while(x<=y) do
+	     t3[x]:=y
+  else 
+	begin
+     if ((x+y-x * x)div y)>=69 then 
+	    X:=x+1-y;
+	end
+  end
+  
+end.
