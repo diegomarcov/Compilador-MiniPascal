f2088aa6acb1361c9e4a5b5713f39c23343e377c
Diego <diegomarcov@gmail.com>
2010-09-01 17:08:14 -0300

    Agregada seccion de errores en el informe. Creo que ya esta listo.

--- Entrega 2/ejemplo1.pas
+++ Entrega 2/ejemplo1.pas
@@ -4,12 +4,12 @@ type entero=integer;
 var A: array{estoy declarando un array{}[(*con cinco (cinco) elementos)*)5] of entero;//el tipo del arreglo es "ENTERO" {
 var VARIABLEDETEST : integer;
 var subRANgo:10..30;
-var a:entero;
+var a1:entero;
 var c:char;
 //asdasad
 {comentario con {llaves(*}
 begin
-a:=100a+Z;
+a:='a'+Z;
 A[2]:=a;
 c:='d';
 
