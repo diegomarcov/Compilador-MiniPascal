383fc7a9fd357ce49de6786d1a24b614ec94a284
Diego <diegomarcov@gmail.com>
2010-09-01 23:41:17 -0300

    Nombres representativos para ejemplos, y un ejemplo_full.pas con TODOTOTE

---
+++ Entrega 2/comentarios.pas
@@ -0,0 +1,16 @@
+prOgrAm ejeMplo1;
+const Z=4;
+type entero=integer;
+var A: array{estoy declarando un array{}[(*con cinco (cinco) elementos)*)5] of entero;//el tipo del arreglo es "ENTERO" {
+var VARIABLEDETEST : integer;
+var subRANgo:10..30;
+var a1:entero;
+var c:char;
+//asdasad
+{comentario con {llaves(*}
+begin
+a:='a'+Z;
+A[2]:=a;
+c:='d';
+
+end.//termino el programa
